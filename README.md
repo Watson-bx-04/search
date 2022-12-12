7136ad6fff2f5f30d8ec15ac81c5a372e38678f4
Watson-bx-04 <119657993+Watson-bx-04@users.noreply.github.com>
2022-12-12 14:58:22 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Web Chat
+
+Qiita Advent Calendar 2022
+
+関連記事
+
+[FAQチャットボットを一歩前進させる 〜 Discoveryとつなぐ]()
---
+++ webchat.js
@@ -0,0 +1,31 @@
+window.watsonAssistantChatOptions = {
+  integrationID: "***",
+  region: "***",
+  serviceInstanceID: "***",
+  clientVersion: "6.2.0",
+  onLoad: async (instance) => {
+    instance.updateCSSVariables({
+      'BASE-width': '360px',
+    });
+    instance.updateHomeScreenConfig({
+      greeting: 'こんにちは！ ピザに関するお問い合わせについてお答えできます',
+      starters: {
+        buttons: [
+          {label: 'ピザの注文'},
+          {label: '割引条件'},
+          {label: '注文のキャンセル'},
+          {label: 'ピザの日はいつ'},
+          {label: 'どこが宅配ピザを始めたの'},
+          {label: 'ピザの語源は'},
+        ],
+      },
+    });
+    await instance.updateLocale('ja');
+    instance.render();
+  }
+};
+setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
+});
